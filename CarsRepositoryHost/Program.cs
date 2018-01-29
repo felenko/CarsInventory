b80e87871569139df0d98605ea2907f3ae4234db
Kostiantyn Felenko <felenko@gmai.com>
2018-01-29 13:16:52 -0500

    Add project files.

---
+++ CarsRepositoryHost/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CarsRepositoryHost
+{
+    
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                CarsRepository.Program.Main(args);
+            }
+        }
+    }
+
